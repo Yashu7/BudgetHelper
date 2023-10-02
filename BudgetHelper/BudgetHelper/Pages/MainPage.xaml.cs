46d0af31062a87b8d029996c36263ba8fe83d620
Yashu7 <janmalawski2@gmail.com>
2023-10-02 22:42:29 +0200

    Added frame styles, deleted animation from mainpage

--- BudgetHelper/BudgetHelper/Pages/MainPage.xaml.cs
+++ BudgetHelper/BudgetHelper/Pages/MainPage.xaml.cs
@@ -15,20 +15,6 @@ namespace BudgetHelper.Pages
         public MainPage()
         {
             InitializeComponent();
-            Task.Run(AnimateBackground);
-        }
-        private async void AnimateBackground()
-        {
-            Action<double> forward = input => bdGradient.AnchorY = input;
-            Action<double> backward = input => bdGradient.AnchorY = input;
-
-            while (true)
-            {
-                bdGradient.Animate(name: "forward", callback: forward, start: 0, end: 1, length: 5000, easing: Easing.SinIn);
-                await Task.Delay(5000);
-                bdGradient.Animate(name: "backward", callback: backward, start: 1, end: 0, length: 5000, easing: Easing.SinIn);
-                await Task.Delay(5000);
-            }
         }
     }
 }
