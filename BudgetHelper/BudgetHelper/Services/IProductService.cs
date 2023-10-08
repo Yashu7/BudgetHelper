ca18871b598849011b979b2003fb4152d8d065f8
Yashu7 <janmalawski2@gmail.com>
2023-10-09 00:15:51 +0200

    IProductService CRUD

---
+++ BudgetHelper/BudgetHelper/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using BudgetHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetHelper.Services
+{
+    public interface IProductService
+    {
+        Task<List<ProductItem>> GetProducts();
+        Task<ProductItem> PostProducts(List<ProductItem> products);
+        Task<int> PutProduct(ProductItem item);
+        Task<int> DeleteProduct(int itemId);
+    }
+}
