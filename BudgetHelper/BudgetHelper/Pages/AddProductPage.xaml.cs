ab3d445b624db4f64a85a6184dc8a3fedbb061c2
Yashu7 <janmalawski2@gmail.com>
2023-10-12 00:03:38 +0200

    Add new product

--- BudgetHelper/BudgetHelper/Pages/AddProductPage.xaml.cs
+++ BudgetHelper/BudgetHelper/Pages/AddProductPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using BudgetHelper.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
--- BudgetHelper/BudgetHelper/ViewModels/AddProductPageModel.cs
+++ BudgetHelper/BudgetHelper/ViewModels/AddProductPageModel.cs
@@ -1,15 +1,45 @@
-﻿using FreshMvvm;
+﻿using BudgetHelper.Models;
+using BudgetHelper.Services;
+using FreshMvvm;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace BudgetHelper.ViewModels
 {
     class AddProductPageModel : FreshBasePageModel
     {
-        public AddProductPageModel()
+        public Command SaveProductCommand { get; set; }
+        private ProductItem _newProduct = new ProductItem();
+        public ProductItem NewProduct
         {
+            get
+            {
+                return _newProduct;
+            }
+            set
+            {
+                _newProduct = value;
+                RaisePropertyChanged("NewProduct");
+            }
+        }
+        private readonly IProductService _productService;
+        public AddProductPageModel(IProductService productServicec)
+        {
+            _productService = productServicec;
+            InitializeCommands();
+        }
 
+        private void InitializeCommands()
+        {
+            SaveProductCommand = new Command(AddProduct);
+        }
+        private async void AddProduct()
+        {
+            await _productService.PutProduct(null);
         }
     }
 }
